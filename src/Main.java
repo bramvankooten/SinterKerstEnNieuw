415040eb73a12bb93e10bb320c2fe0d3be50199d
Bram van Kooten <bram.vankooten@hotmail.com>
2017-12-14 11:22:08 +0100

    Start of ui

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Stage mainStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
+//        mainStage.setScene(æ);
+    }
+}
---
+++ src/MainMenuController.java
@@ -0,0 +1,6 @@
+public class MainMenuController {
+
+    public void quitButtonClicked() {
+
+    }
+}
